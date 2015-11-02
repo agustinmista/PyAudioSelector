00bf8e162e205e11a93cb307931f6274f4cf8a1d
Agustín Mista <mista.agustin@gmail.com>
2015-11-02 20:51:51 -0300

    Added installer script

--- PyAudioSelector.py
+++ PyAudioSelector.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
@@ -14,53 +15,53 @@ except:
 
 
 class AudioSelector:
-    
+
     def __init__(self, parser):
-        
+
         # Parse the config file
         self.parser = parser
         self.parse_config()
-        
+
         # Create the indicator
         self.ind = AppIndicator.Indicator.new("PyAudioSelector",
                         self.indicator_icon,AppIndicator.IndicatorCategory.OTHER)
         self.ind.set_status(AppIndicator.IndicatorStatus.ACTIVE)
-        
+
         # Get PulseAudio status
         self.default_device, self.avaiable_devices, self.inputs = self.get_audio_status()
-        
+
         # Create the menu accordingly, and check periodically for changes
         self.create_menu()
         GLib.timeout_add_seconds(self.refresh_interval, self.handler_check_refresh)
-        
-        
+
+
     def get_audio_status(self):
         default_device, avaiable_devices = getPulseAudioDevices()
         inputs = getPulseAudioInputs()
         return (default_device, avaiable_devices, inputs)
-        
-        
-        
+
+
+
     def parse_config(self):
         self.refresh_interval  = int(parser.get('constants', 'refresh_interval'))
         self.connected_icon    = parser.get('constants', 'connected_icon')
         self.disconnected_icon = parser.get('constants', 'disconnected_icon')
         self.indicator_icon    = parser.get('constants', 'indicator_icon')
         self.settings_command  = parser.get('constants', 'sound-settings-command')
-    
-    
+
+
     def create_menu(self):
         self.menu = Gtk.Menu()
-        
+
         # Create the applications section (if any)
         if self.inputs:
-            
+
             # Applications label
             item = Gtk.MenuItem("Applications")
             item.set_sensitive(False)
             item.show()
             self.menu.append(item)
-        
+
             # Add a menu entry for each audio input
             for in_id, in_name, in_icon, in_sink in self.inputs:
                 item = Gtk.ImageMenuItem.new_from_stock(in_icon, None)
@@ -69,13 +70,13 @@ class AudioSelector:
                 # Add a submenu to select audio device
                 submenu = Gtk.Menu()
                 item.set_submenu(submenu)
-                
+
                 # Applications label
                 sub_item = Gtk.MenuItem("Play on...")
                 sub_item.set_sensitive(False)
                 sub_item.show()
                 submenu.append(sub_item)
-                
+
                 for dev_id, dev_name in self.avaiable_devices:
                     sub_item = Gtk.ImageMenuItem.new_from_stock(self.disconnected_icon, None)
                     sub_item.set_label(dev_name)
@@ -95,15 +96,15 @@ class AudioSelector:
             item = Gtk.SeparatorMenuItem()
             item.show()
             self.menu.append(item)
-        
-        
+
+
         # Devices label
         if self.avaiable_devices:
             item = Gtk.MenuItem("Devices")
             item.set_sensitive(False)
             item.show()
             self.menu.append(item)
-        
+
         # Add a menu entry for each audio device
         for dev_id, dev_name in self.avaiable_devices:
             item = Gtk.ImageMenuItem.new_from_stock(self.disconnected_icon, None)
@@ -111,7 +112,7 @@ class AudioSelector:
             item.connect("activate", lambda w, did: self.handler_switch_all(did), dev_id)
             item.show()
             self.menu.append(item)
-            
+
             if self.inputs:
                 # Disable device if all inputs are seted to this device and it's the default_device
                 has_all_inputs = all(in_sink == dev_id for in_sink in [inp[3] for inp in self.inputs])
@@ -123,36 +124,36 @@ class AudioSelector:
                 if dev_id == self.default_device:
                     item.set_sensitive(False)
                     item.set_image(Gtk.Image.new_from_stock(self.connected_icon, Gtk.IconSize.MENU))
-                
-                
+
+
         # Separator
         item = Gtk.SeparatorMenuItem()
         item.show()
         self.menu.append(item)
-        
+
         # Open the sound settings
         item = Gtk.ImageMenuItem.new_from_stock(Gtk.STOCK_PREFERENCES, None)
         item.connect("activate", lambda w: self.handler_open_settings())
         item.show()
-        self.menu.append(item)        
-        
+        self.menu.append(item)
+
         # Refresh the menu
         item = Gtk.ImageMenuItem.new_from_stock(Gtk.STOCK_REFRESH, None)
         item.connect("activate", lambda w: self.handler_refresh_menu())
         item.show()
         self.menu.append(item)
-        
+
         # Exit the app
         item = Gtk.ImageMenuItem.new_from_stock(Gtk.STOCK_QUIT, None)
         item.connect("activate", lambda w: self.handler_menu_exit())
         item.show()
         self.menu.append(item)
-		
+
 		# Show the indicator
         self.menu.show()
         self.ind.set_menu(self.menu)
-        
-        
+
+
     def handler_switch_in(self, in_id, dev_id):
         # Move sink input to the selected device
         cmd = 'pactl move-sink-input ' + in_id + ' ' + dev_id
@@ -160,71 +161,71 @@ class AudioSelector:
         Popen(cmd, shell=True, stdout=PIPE).communicate()
         self.handler_refresh_menu()
 
-        
+
     def handler_switch_all(self, dev_id):
         # Move sink inputs to the selected device
         for input_id,_,_,_ in getPulseAudioInputs():
             cmd = 'pactl move-sink-input ' + input_id + ' ' + dev_id
             print 'PulseAudio:   ' + cmd
             Popen(cmd, shell=True, stdout=PIPE).communicate()
-        
+
         # Set selected device as default
         cmd = 'pacmd set-default-sink ' + dev_id
         print 'PulseAudio:   ' + cmd
         Popen(cmd, shell=True, stdout=PIPE).communicate()
-        
+
         self.handler_refresh_menu()
-        
-        
+
+
     def handler_refresh_menu(self):
         self.default_device, self.avaiable_devices, self.inputs = self.get_audio_status()
         self.menu.destroy()
         self.create_menu()
 
-        
+
     def handler_check_refresh(self):
         # Get current status
         tmp_dd, tmp_ad, tmp_i = self.get_audio_status()
-        
+
         # If something changed, refresh the menu
-        if(tmp_dd != self.default_device or 
-           tmp_ad != self.avaiable_devices or 
+        if(tmp_dd != self.default_device or
+           tmp_ad != self.avaiable_devices or
            tmp_i  != self.inputs):
             self.default_device, self.avaiable_devices, self.inputs = tmp_dd, tmp_ad, tmp_i
             self.menu.destroy()
             self.create_menu()
-        
+
         # Finally, reset the timer
         GLib.timeout_add_seconds(self.refresh_interval, self.handler_check_refresh)
-        
-        
+
+
     def handler_open_settings(self):
          Popen(self.settings_command, shell=True, stdout=PIPE).communicate()
 
-            
+
     def handler_menu_exit(self):
         Gtk.main_quit()
-		
-        
+
+
     def main(self):
         Gtk.main()
 
-        
+
 def getPulseAudioDevices():
     raw, _ = Popen('pacmd list-sinks', shell=True, stdout=PIPE).communicate()
-    
+
     raw_devices = findall(r"(\*?) index: (\d+)", str(raw))
     names = findall(r"device.description = \"(\S.+)\"", str(raw))
-    
+
     devices = [tup[1] for tup in raw_devices]
     default = [x[1] for x in raw_devices if x[0] is '*'][0]
-    
+
     return (default, zip(devices, names))
 
 
 def getPulseAudioInputs():
     raw_inputs, _ = Popen('pacmd list-sink-inputs', shell=True, stdout=PIPE).communicate()
-    
+
     input_id = findall(r"index: (\d+)", str(raw_inputs))
     input_app_name = findall(r"application.process.binary = \"(\S.+)\"", str(raw_inputs))
     input_app_icon = findall(r"application.icon_name = \"(\S.+)\"", str(raw_inputs))
@@ -233,9 +234,16 @@ def getPulseAudioInputs():
     return zip(input_id, input_app_name, input_app_icon, input_sink)
 
 if __name__ == "__main__":
-    
-    config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
-    
+
+    if os.path.dirname(__file__) == "/usr/local/bin":
+        config_file = "/usr/share/PyAudioSelector/config.ini"
+    elif os.path.dirname(__file__) == ".":
+        config_file = "config.ini"
+    else:
+        print "You do not have a working installation of PyAudioSelector"
+        print "See the installation procedure in the README file"
+        sys.exit(1)
+
     parser = SafeConfigParser()
     parser.read(config_file)
 
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USER_HOME=$(eval echo ~${SUDO_USER})
+
+# Copy files to path
+mkdir -p /usr/share/PyAudioSelector
+chmod +x PyAudioSelector.py
+cp PyAudioSelector.py /usr/local/bin/PyAudioSelector
+cp config.ini /usr/share/PyAudioSelector
+
+# Copy the autostart entry
+cp PyAudioSelector.desktop ${USER_HOME}/.config/autostart/
+
+echo "Installation completed"
